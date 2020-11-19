9dad1707f81211af116ebd88fe66eba81ef02780
frontkot <62297860+frontkot@users.noreply.github.com>
2020-11-19 13:26:38 +0200

    create component Loader (#3)
    * create component Loader
    
    * delete .gitkeep from components main repository

---
+++ client/src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.scss";
+
+const Loader = () => (
+  <div className="loader-window">
+    <div className="loader-motion">
+    </div>
+    <div className="loader-static">
+    </div>
+  </div>
+);
+
+export default Loader;
---
+++ client/src/components/Loader/Loader.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Loader from "./Loader";
+
+describe("Unit testing Loader", () => {
+    test("smoke test Loader", () => {
+        render(<Loader />);
+    });
+});
