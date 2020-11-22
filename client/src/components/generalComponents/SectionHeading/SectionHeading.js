dac7e7c15db3dba3679280752532c67103a8a8ad
olha-s <69920194+olha-s@users.noreply.github.com>
2020-11-22 13:57:03 +0200

    Section heading (#17)
    * creating variables color
    
    * section-heading
    
    * SectionHeading and FilterFieldTitle
    
    * deleted commented variables colors
    
    Co-authored-by: zayats.ua <64593495+RomanZayats@users.noreply.github.com>

---
+++ client/src/components/generalComponents/SectionHeading/SectionHeading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./SectionHeading.scss";
+
+export const SectionHeading = ({text}) => {
+    return (
+        <h2 data-testid="section-heading" className="section__heading">{text}</h2>
+    );
+};
+
+SectionHeading.propTypes = {
+    text: PropTypes.string.isRequired
+};
+
+export default SectionHeading;
---
+++ client/src/components/generalComponents/SectionHeading/SectionHeading.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import SectionHeading from "./SectionHeading";
+
+const mockText = "Title";
+
+test("SectionHeading is rendered is correctly", () => {
+  render(<SectionHeading text={mockText} />);
+});
+test("Simple SectionHeading snapshot", () => {
+  const { container } = render(<SectionHeading text={mockText} />);
+  expect(container.innerHTML).toMatchInlineSnapshot(
+    `"<h2 data-testid=\\"section-heading\\" class=\\"section__heading\\">Title</h2>"`
+  );
+});
+
+test("SectionHeading contains text", () => {
+  const { getByTestId, getByText } = render(<SectionHeading text={mockText} />);
+  const title = getByTestId("section-heading");
+
+  expect(getByText("Title")).toBeInTheDocument();
+  expect(title).toBeDefined();
+});
