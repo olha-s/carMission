ab0759053e872fcc19dd0c3e57d66513ce8df2e1
zayats.ua <64593495+RomanZayats@users.noreply.github.com>
2020-12-02 21:46:37 +0200

    About us and auto from usa (#68)
    * updated aboutUS and styles for socialNetworks in autoFromUSA
    
    * updated autoFromUSA

--- client/src/sections/AutoFromUSA/AutoFromUSA.test.js
+++ client/src/sections/AutoFromUSA/AutoFromUSA.test.js
@@ -9,23 +9,27 @@ const mockHeadingText = "Test heading";
 const mockSectionClassName = "auto-from-usa__container";
 const mockSectionDescription = "Test description";
 const mockStore = configureStore();
-const store = mockStore({feedbackFormOpen: "closed"});
+const store = mockStore({ feedbackFormOpen: "closed" });
 
 test("AutoFromUsa is rendered correctly", () => {
   render(
-   <Provider store={store}> <AutoFromUsa className={mockSectionClassName} heading={mockHeadingText} /></Provider>
+    <Provider store={store}>
+      <AutoFromUsa className={mockSectionClassName} heading={mockHeadingText} />
+    </Provider>
   );
 });
 
 test("AutoFromUsa contains elements", () => {
   const { getByTestId } = render(
-   <Provider store={store}> <AutoFromUsa className={mockSectionClassName} heading={mockHeadingText}>
-      <div className="auto-from-usa__wrapper">
-        <h1 className="auto-from-usa__heading">{mockHeadingText}</h1>
-        <p className="auto-from-usa__description">{mockSectionDescription}</p>
-        <Button text="testBtn"/>
-      </div>
-    </AutoFromUsa> </Provider>
+    <Provider store={store}>
+      <AutoFromUsa className={mockSectionClassName} heading={mockHeadingText}>
+        <div className="auto-from-usa__wrapper">
+          <h1 className="auto-from-usa__heading">{mockHeadingText}</h1>
+          <p className="auto-from-usa__description">{mockSectionDescription}</p>
+          <Button text="testBtn" />
+        </div>
+      </AutoFromUsa>
+    </Provider>
   );
   const btnText = getByTestId("btn");
   expect(btnText).toBeDefined();
--- client/src/sections/AutoFromUSA/AutoFromUsa.js
+++ client/src/sections/AutoFromUSA/AutoFromUsa.js
@@ -1,34 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./AutoFromUSA.scss";
 import Button from "../../components/generalComponents/Button/Button";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { showFeedbackFormAction } from "../../store/FeedbackForm/actions";
+import SocialNetworks from "../../components/SocialNetworks/SocialNetworks";
 
-const AutoFromUsa = () => {
-  const [heading, setHeading] = useState("");
-  const [description, setDescription] = useState("");
-  // const [imgPath, setImgPath] = useState("");
-
+const AutoFromUsa = ({ heading, description, anchorName }) => {
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    const dataFromServer = await axios("/api/sections-main/").then((res) => {
-      return res.data.filter((d) => d.index === 1);
-    });
-    setHeading(dataFromServer[0].heading);
-    setDescription(dataFromServer[0].description);
-    // setImgPath(dataFromServer[0].imgPath);
-  };
-
   return (
-    <section className="auto-from-usa__container">
+    <section className="auto-from-usa__container" id={anchorName}>
       <div className="auto-from-usa__wrapper">
         <h1 className="auto-from-usa__heading">{heading}</h1>
+        <SocialNetworks className="header__networks" />
         <p className="auto-from-usa__description">{description}</p>
         <Button
           className="button-choose-car"
@@ -41,5 +24,4 @@ const AutoFromUsa = () => {
     </section>
   );
 };
-
 export default AutoFromUsa;
