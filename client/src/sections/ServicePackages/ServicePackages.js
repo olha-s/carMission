06cab166e304e242eaedd6e5b95ae960474c34ac
bachik85 <61582695+bachik85@users.noreply.github.com>
2021-02-03 23:01:40 +0200

    Update ServicePackages.js (#134)
    * Update ServicePackages.js
    
    slider
    
    * Update ServicePackages.js
    
     fix

--- client/src/sections/ServicePackages/ServicePackages.js
+++ client/src/sections/ServicePackages/ServicePackages.js
@@ -5,19 +5,27 @@ import Button from "../../components/generalComponents/Button/Button";
 import ServicePackage from "./components/ServicePackage";
 import { showFeedbackFormAction } from "../../store/FeedbackForm/actions";
 import { useDispatch, useSelector } from "react-redux";
-import { getPackages } from "../../store/servicePackages/selectors";
+import {
+  getPackages,
+  getPackagesIsLoading,
+} from "../../store/servicePackages/selectors";
 import useLiveHashPush from "../../utils/hooks/useLiveHashPush";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Loader from "../../components/Loader/Loader";
 
-const ServicePackages = ({ heading, anchorName, description }) => {
+const ServicePackages = ({ heading, anchorName }) => {
   const dispatch = useDispatch();
   const ref = useLiveHashPush(anchorName);
-  const servicePackages = useSelector(getPackages);
+  const services = useSelector(getPackages);
+  const isLoading = useSelector(getPackagesIsLoading);
 
   const showFeedbackModal = () => {
     dispatch(showFeedbackFormAction);
   };
 
-  const servicePackagesToRender = servicePackages.map((servicePackage) => {
+  const servicePackagesToRender = services.map((servicePackage) => {
     const { price, currency, serviceList, name, _id: id } = servicePackage;
     return (
       <ServicePackage
@@ -31,16 +39,65 @@ const ServicePackages = ({ heading, anchorName, description }) => {
     );
   });
 
+  const settings = {
+    className: "center",
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 898,
+        settings: {
+          dots: false,
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          arrows: false,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="service-packages" id={anchorName} ref={ref}>
       <SectionHeading text={heading} />
-      <div className="service-packages__wrapper">{servicePackagesToRender}</div>
-      <p className="service-packages__description">{description}</p>
-      <Button
-        className="button2-send-request"
-        text="Отправить заявку"
-        onClick={showFeedbackModal}
-      />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="service-packages__wrapper">
+          <Slider {...settings}>{servicePackagesToRender}</Slider>
+          <Button
+            className="button2-send-request"
+            text="Отправить заявку"
+            onClick={showFeedbackModal}
+          />
+        </div>
+      )}
     </section>
   );
 };
